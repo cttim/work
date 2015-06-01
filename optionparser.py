c7488e754598c5968f7e9268fed7e8a823c5b813
tim <cttimcht@gmail.com>
2015-06-01 17:47:53 +0800

    update optionparser

--- optionparser.py
+++ optionparser.py
@@ -3,17 +3,12 @@ import pyodbc
 constr = 'DSN=hana;UID=SYSTEM;PWD=manager'
 
 class Optionparser():
-  def __init__(self, option):
+  def __init__(self, option, cur):
     self.options = 'dict(' + option + ')'
+    self.cursor=cur
     
   def executeoption(self):
-    # connect to hanadb
-    con = pyodbc.connect(constr)
-    cur = con.cursor()
-
-    # set the scheme
-    cur.execute("SET SCHEMA DM_PAL")
-    
+    cur = self.cursor
     # create control table
     try:
       cur.execute("DROP TABLE #PAL_CONTROL_TBL")
@@ -33,19 +28,16 @@ class Optionparser():
     for key in test:
       value = test[key]
       if(isinstance(value, float)):
-        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ({0},NULL,{1},NULL)".format("'"+key+"'", value)
-        print(executestring)
+        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ('{0}',NULL,{1},NULL)".format(key, value)
         cur.execute(executestring)
       elif(isinstance(value, int)):
-        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ({0},{1},NULL,NULL)".format("'"+key+"'", value)
-        print(executestring)
+        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ('{0}',{1},NULL,NULL)".format(key, value)
         cur.execute(executestring)
       elif(isinstance(value, str)):
-        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ({0},NULL,NULL,{1})".format("'"+key+"'", value)
-        print(executestring)
+        executestring = "INSERT INTO #PAL_CONTROL_TBL VALUES ('{0}',NULL,NULL,'{1}')".format(key, value)
         cur.execute(executestring)
       else:
-        print("error option")
+        raise Exception('incorrect option, check option format')
     cur.commit()
       
 
